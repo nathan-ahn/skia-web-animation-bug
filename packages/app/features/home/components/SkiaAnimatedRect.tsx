f6751d5ed4ea1ff3a766f2bd562defeef825b26b
Nathan Ahn <nathancahn@gmail.com>
2025-02-05 14:56:40 -0500

    Web animations broken demo

--- packages/app/features/home/components/SkiaAnimatedRect.tsx
+++ packages/app/features/home/components/SkiaAnimatedRect.tsx
@@ -1,10 +1,46 @@
 import { Canvas, Rect } from '@shopify/react-native-skia'
+import { useEffect } from 'react'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated'
 
 const SkiaAnimatedRect = () => {
+  const x = useSharedValue(0)
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(100, { duration: 1000 }),
+        withTiming(0, { duration: 1000 })
+      ),
+      -1
+    )
+  }, [x])
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: x.value }],
+    }
+  }, [x])
+
   return (
-    <Canvas>
-      <Rect x={0} y={0} width={100} height={100} color="blue" />
-    </Canvas>
+    <>
+      <Canvas>
+        <Rect x={x} y={0} width={100} height={100} color="blue" />
+      </Canvas>
+      <Animated.View
+        style={[
+          animatedStyle,
+          {
+            width: 100,
+            height: 100,
+            backgroundColor: 'pink',
+          },
+        ]}
+      ></Animated.View>
+    </>
   )
 }
 export default SkiaAnimatedRect
